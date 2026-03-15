f80addb667405248e049363788d25bd6fe6aeaa4
Your Name <your.email@example.com>
2026-03-15 22:55:38 +0530

    add link

--- server/index.js
+++ server/index.js
@@ -10,21 +10,22 @@ const { registerDrawHandlers } = require('./handlers/drawHandler');
 
 const app = express();
 const server = http.createServer(app);
+const io = new Server(server)
 
-const io = new Server(server, {
-  cors: {
-    origin: [
-      'http://localhost:5173',
-      'http://localhost:5174',
-      process.env.CLIENT_URL || 'https://skribbl-client.onrender.com',
-      'https://scribble.vidyarupi.com/', '0.0.0.0'
+// const io = new Server(server, {
+//   cors: {
+//     origin: [
+//       'http://localhost:5173',
+//       'http://localhost:5174',
+//       process.env.CLIENT_URL || 'https://skribbl-client.onrender.com',
+//       'https://scribble.vidyarupi.com/', '0.0.0.0'
 
-    ].filter(Boolean),
-    methods: ['GET', 'POST'],
-  },
-});
+//     ].filter(Boolean),
+//     methods: ['GET', 'POST'],
+//   },
+// });
 
-app.use(cors());
+app.use(cors(''));
 app.use(express.json());
 
 // Health check
